681478b9d1e6c9a7efdd32385b599a40f519da45
Eisak-23 <iolikapi10@gmail.com>
2024-04-23 16:10:45 -0500

    db blockchain

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
-use std::time::SystemTime;
 
+use std::time::SystemTime;
 use crypto::{digest::Digest, sha2::Sha256};
+use serde::{Deserialize, Serialize};
 
 pub type Result<T> = std::result::Result<T, failure::Error>;
 
 const  TARGET_HEIGTH: usize  = 4;
 
-#[derive(Debug, Clone)]
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Block {
     timestamp:u128,
     trasactions:String,
@@ -14,6 +16,12 @@ pub struct Block {
     hash:String,
     height:usize,
     nonce:u32,
+}
+
+impl  Block {
+    pub(crate) fn get_prev_hash(&self) -> String{
+       self.prev_hash.clone()
+    }
 }
 
 
@@ -90,39 +98,118 @@ impl Block {
 
 
 
-
-#[derive(Debug, Clone)]
+#[derive(Debug,Clone)]
 pub struct Blockchain {
-    blocks: Vec<Block>
+    current_hash:String,
+    db:sled::Db
+    
 }
+
+
+struct BlockchainIter<'a>  {
+    current_hash: String,
+    cb: &'a Blockchain, 
+}
+
+
 impl  Blockchain {
-    pub fn new() -> Blockchain {
-        Blockchain {
-            blocks: vec![Block::new_genesiss()]
+    pub fn new() -> Result<Blockchain> {
+        let db = sled::open("/data/blocks").unwrap();
+        match db.get("LAST")? {
+            Some(hash) => {
+                let lasthash =  String::from_utf8(hash.to_vec())?;
+                Ok(Blockchain {
+                    current_hash: lasthash,
+                    db,
+                })
+
+            }
+            None => {
+                let block = Block::new_genesiss();
+                db.insert(block.get_hash(), bincode::serialize(&block)?)?;
+                db.insert("LAST", block.get_hash().as_bytes())?;
+                let bc = Blockchain {
+                    current_hash: block.get_hash(),
+                    db
+                };
+
+                bc.db.flush()?;
+                Ok(bc)
+
+
+
+            }
+
         }
+       
     }
     
     pub fn add_block(&mut self, data: String) -> Result<()> {
-        let prev  = self.blocks.last().unwrap();
-        let new_block = Block::new_block(data, prev.get_hash(), TARGET_HEIGTH)?;
-        self.blocks.push(new_block);
+        let lashash = self.db.get("LAST")?.unwrap();
+        let new_block = Block::new_block(data, String::from_utf8( lashash.to_vec())?, TARGET_HEIGTH)?;
+        self.db.insert(new_block.get_hash(),  bincode::serialize(&new_block)?)?;
+        self.db.insert("LAST", new_block.get_hash().as_bytes())?;
+
+        self.current_hash = new_block.get_hash();
         Ok(())
 
     }
 
+    pub fn iter(&self) -> BlockchainIter {
+        BlockchainIter {
+            current_hash: self.current_hash.clone(),
+            cb: &self
+        }
+        
+    }
+
+
 }
+
+
+impl<'a> Iterator for BlockchainIter<'a> {
+    type Item = Block;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let  Ok(encode_block) =  self.cb.db.get(&self.current_hash) {
+            return  match encode_block {
+                Some(b) => {
+                    if let Ok(block) = bincode::deserialize::<Block>(&b) {
+                        self.current_hash = block.get_prev_hash();
+                        Some(block)
+                    }else {
+                        None
+                    }
+                }
+               None => None 
+            };
+        }
+    None
+    }
+    
+}
+
 
 
 #[cfg(test)]
 mod test {
+    use colored::Colorize;
+
     use super::*;
     #[test]
     fn test_blockchain() {
-        let mut b = Blockchain::new();
+        let mut b = Blockchain::new().unwrap();
         b.add_block("data".to_string());
         b.add_block("data1".to_string());
         b.add_block("data2".to_string());
-        dbg!(b);
+        
+
+        for item  in  b.iter() {
+            
+            let colored_item = format!("{:?}", item.trasactions).green();
+            println!("Item: {}", colored_item);
+        }
+
 
     }
 
